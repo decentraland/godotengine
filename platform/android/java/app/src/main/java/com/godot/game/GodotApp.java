42e3c0ad27b8f95458a5095a38937a5b4ec70358
Lean Mendoza <8042536+leanmendoza@users.noreply.github.com>
2025-10-16 07:37:14 -0300

    add support to etcpak on runtime adjust ci to build our editor/templates customize android build to run with our gdext lib
    squashed from mateo's commits

--- platform/android/java/app/src/main/java/com/godot/game/GodotApp.java
+++ platform/android/java/app/src/main/java/com/godot/game/GodotApp.java
@@ -45,6 +45,9 @@ import androidx.core.splashscreen.SplashScreen;
  */
 public class GodotApp extends GodotActivity {
 	static {
+		// This block calls the JNI_OnLoad, needed for livekit 
+		System.loadLibrary("c++_shared");
+		System.loadLibrary("dclgodot");
 		// .NET libraries.
 		if (BuildConfig.FLAVOR.equals("mono")) {
 			try {
