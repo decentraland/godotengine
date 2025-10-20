6120dcbbbaf71d4ec13349e3905d9d6a0f77d231
Lean Mendoza <8042536+leanmendoza@users.noreply.github.com>
2025-10-20 11:46:38 -0300

    fix deep link

--- platform/android/export/gradle_export_util.cpp
+++ platform/android/export/gradle_export_util.cpp
@@ -305,6 +305,13 @@ String _get_activity_tag(const Ref<EditorExportPlatform> &p_export_platform, con
 
 	manifest_activity_text += "            </intent-filter>\n";
 
+	manifest_activity_text += "            <intent-filter>\n"
+							"                <action android:name=\"android.intent.action.VIEW\" />\n"
+							"                <category android:name=\"android.intent.category.DEFAULT\" />\n";
+							"                <category android:name=\"android.intent.category.BROWSABLE\" />\n";
+							"                <data android:scheme=\"decentraland\" />\n";
+	manifest_activity_text += "            </intent-filter>\n";
+
 	Vector<Ref<EditorExportPlugin>> export_plugins = EditorExport::get_singleton()->get_export_plugins();
 	for (int i = 0; i < export_plugins.size(); i++) {
 		if (export_plugins[i]->supports_platform(p_export_platform)) {
