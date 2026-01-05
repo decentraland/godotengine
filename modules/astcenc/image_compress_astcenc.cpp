1e8d674ff35dfa89fd5dc78497655b0f11ad9230
Mateo "Kuruk" Miccino <mateomiccino@gmail.com>
2026-01-05 10:58:52 -0300

    feat: enable ASTC compression for non-editor builds
    Remove TOOLS_ENABLED guards and ASTCENC_DECOMPRESS_ONLY flag to allow
    ASTC texture compression at runtime, not just in the editor.

--- modules/astcenc/image_compress_astcenc.cpp
+++ modules/astcenc/image_compress_astcenc.cpp
@@ -35,7 +35,6 @@
 
 #include <astcenc.h>
 
-#ifdef TOOLS_ENABLED
 void _compress_astc(Image *r_img, Image::ASTCFormat p_format) {
 	const uint64_t start_time = OS::get_singleton()->get_ticks_msec();
 
@@ -172,7 +171,6 @@ void _compress_astc(Image *r_img, Image::ASTCFormat p_format) {
 
 	print_verbose(vformat("astcenc: Encoding took %d ms.", OS::get_singleton()->get_ticks_msec() - start_time));
 }
-#endif // TOOLS_ENABLED
 
 void _decompress_astc(Image *r_img) {
 	const uint64_t start_time = OS::get_singleton()->get_ticks_msec();
--- modules/astcenc/image_compress_astcenc.h
+++ modules/astcenc/image_compress_astcenc.h
@@ -32,8 +32,5 @@
 
 #include "core/io/image.h"
 
-#ifdef TOOLS_ENABLED
 void _compress_astc(Image *r_img, Image::ASTCFormat p_format);
-#endif
-
 void _decompress_astc(Image *r_img);
