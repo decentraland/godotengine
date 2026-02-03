fc8513852910cbd3f17a629e8343942d29816ec9
Lean Mendoza <8042536+leanmendoza@users.noreply.github.com>
2026-02-03 10:54:50 -0300

    fix add app link android (by exporting)

--- platform/android/export/gradle_export_util.cpp
+++ platform/android/export/gradle_export_util.cpp
@@ -356,6 +356,14 @@ String _get_activity_tag(const Ref<EditorExportPlatform> &p_export_platform, con
 							  "                <data android:scheme=\"decentraland\" />\n"
 							  "            </intent-filter>\n";
 
+	// App Links for mobile.dclexplorer.com
+	manifest_activity_text += "            <intent-filter android:autoVerify=\"true\">\n"
+							  "                <action android:name=\"android.intent.action.VIEW\" />\n"
+							  "                <category android:name=\"android.intent.category.DEFAULT\" />\n"
+							  "                <category android:name=\"android.intent.category.BROWSABLE\" />\n"
+							  "                <data android:scheme=\"https\" android:host=\"mobile.dclexplorer.com\" />\n"
+							  "            </intent-filter>\n";
+
 	// Hybrid categories should only go to the actual 'GodotApp' activity.
 	Ref<RegEx> activity_alias_content_to_remove_regex = RegEx::create_from_string(R"delim(<category\s+android:name\s*=\s*"org.godotengine.xr.hybrid.(IMMERSIVE|PANEL)"\s*\/>)delim");
 	String updated_export_plugins_activity_alias_element_contents = activity_alias_content_to_remove_regex->sub(export_plugins_activity_element_contents, "", true);
