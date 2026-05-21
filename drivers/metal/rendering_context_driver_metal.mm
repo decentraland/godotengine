28d06ab971278bb674a6fa68e3bc547d7059bf9d
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2026-05-21 14:13:48 +0300

    Disable ubershaders on problematic Adreno compiler versions.
    (cherry picked from commit c6987624efe17ffd78dfe9e8b76eaa4854704ca2)

--- drivers/metal/rendering_context_driver_metal.mm
+++ drivers/metal/rendering_context_driver_metal.mm
@@ -73,7 +73,6 @@ Error RenderingContextDriverMetal::initialize() {
 #endif
 	device.type = DEVICE_TYPE_INTEGRATED_GPU;
 	device.vendor = Vendor::VENDOR_APPLE;
-	device.workarounds = Workarounds();
 
 	MetalDeviceProperties props(metal_device);
 	int version = (int)props.features.highestFamily - (int)MTLGPUFamilyApple1 + 1;
